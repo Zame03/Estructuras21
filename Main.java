dc335a98d88aa37c44dc132464db58ec6c65a99b
Zame03 <bedoyajuan@432@gmail.com>
2026-02-09 09:16:51 -0500

    Sumas

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
-public static void main(String[] args) {
-System.out.println("Hola mundo");
+    public static void main(String[] args) {
+        System.out.println("Vamos a sumar dos numeros");
+        int n1 = 1, n2 = 0, suma = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Ingrese el numero 1: ");
+        n1 = scan.nextInt();
+        System.out.print("Ingrese el numero 2: ");
+        n2 = scan.nextInt();
+
+        suma = n1 + n2;
+        System.out.println("Suma: " + (n1 + n2));
+
+        scan.close();
 
 
 }
